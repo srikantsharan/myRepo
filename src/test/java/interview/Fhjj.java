7c5d7bcb957e678edb63c73fa50d8b5fba6ca0b6
srikant sharan <srikantbihari@gmail.com>
2024-03-03 15:07:41 +0530

    create launchurl method

--- src/test/java/interview/Fhjj.java
+++ src/test/java/interview/Fhjj.java
@@ -1,11 +1,26 @@
 package interview;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Fhjj {
+	
+	static WebDriver driver;
+	
+	public void launchurl()
+	{
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http:www.amazon.in");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hi srikant");
+	Fhjj obj=new Fhjj();
+	obj.launchurl();
+			
+		
 
 	}
 
